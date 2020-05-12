4552ca71200dbd4e4a1f2b70aab26ac9dca731db
HungVyDS <62836191+VynguyenDS@users.noreply.github.com>
2020-05-12 15:25:12 +0700

    Update
    Update 5File for addtoBag

---
+++ File_PHP/OrderProduct.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+  <head>
+       <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../File_CSS/Mainpage.css">
+    <link rel="stylesheet" type="text/css" href="../File_CSS/Advertisement1.css">
+    <link rel="stylesheet" type="text/css" href="../File_CSS/Advertisement2.css">
+    <link rel="stylesheet" type="text/css" href="../File_CSS/women.css">
+    <link rel="stylesheet" type="text/css" href="../File_CSS/menu_bar.css">
+    <link rel="stylesheet" type="text/css" href="../File_CSS/Login.css">
+    <link rel="stylesheet" type="text/css" href="../File_CSS/OrderProduct.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.6/css/flag-icon.min.css">
+    <title>Đồng hồ nữ</title>
+    <style>
+* {box-sizing: border-box}
+
+.container {
+  width: 100%;
+  background-color: #ddd;
+  height: 60px;
+}
+
+.skills {
+  text-align: right;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  color: white;
+}
+
+.html {width: 90%; background-color: rgb(247,148,29);}
+.css {width: 80%; background-color: rgb(247,148,29);}
+.js {width: 65%; background-color: rgb(247,148,29);}
+.php {width: 60%; background-color: rgb(247,148,29);}
+</style>
+  </head>
+  <body>    
+    <?php include 'PartOfWeb/header_bar.php';?>
+    
+
+    <?php include 'PartOfWeb/menu_bar.php'?>
+
+
+    <div class="addToBag container-fluid">
+        <div class="leftBag">
+            123123123
+
+        </div>
+        <div class="righttBag">
+            <p>
+                <b>FB-01 Three-Hand Date Stainless Steel Watch</b>
+            </p>
+            <div>
+                        <span id="rating">
+                          <span class="fa fa-star checked" style="color: orange;"></span>
+                          <span class="fa fa-star checked" style="color: orange;"></span>
+                          <span class="fa fa-star checked" style="color: orange;"></span>
+                          <span class="fa fa-star"></span>
+                          <span class="fa fa-star"></span>
+                        </span>
+
+            </div>
+            <div><span style="text-decoration: line-through;">1.200.000 </span><span style="color: red;"><b>1.000.000VND</b></span></div>
+            <div id="color"></div>
+            <div id="addtoBag">Thêm vào giỏ</div>
+            <hr>
+            <div>
+                <p>Sản phẩm chi tiết</p>
+                <p>Sản phẩm chi tiết</p>
+                <p>Sản phẩm chi tiết</p>
+                <p>Sản phẩm chi tiết</p>
+                <p>Sản phẩm chi tiết</p>
+                <p>Sản phẩm chi tiết</p>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid" id ='review'>
+        <p>Review Snapshot by <b>PowerReviews</b></p>
+        <div class="row">
+            <div class="col-md-4" style="text-align: center;">
+                    <h3>3.3</h3>
+                    <span id="rating">
+                          <span class="fa fa-star checked" style="color: orange;"></span>
+                          <span class="fa fa-star checked" style="color: orange;"></span>
+                          <span class="fa fa-star checked" style="color: orange;"></span>
+                          <span class="fa fa-star"></span>
+                          <span class="fa fa-star"></span>
+                    </span>
+                    <span>21 người Reviews</span><br>
+                    <button style="background-color: rgb(11,123,193);">Ghi Reviews</button>
+
+            </div>
+            <div class="col-md-4" style="text-align: center;">
+                <h2  ><span style="background-color: rgb(10,137,0);padding: 5px 30px;"> 81%</span></h2>
+                <p>trong số những người được hỏi muốn giới thiệu điều này cho bạn bè</p>
+            </div>
+            <div class="col-md-4">
+    
+<span>HTML</span>
+<div class="container">
+  <div class="skills html">90%</div>
+</div>
+
+<span>CSS</span>
+<div class="container">
+  <div class="skills css">80%</div>
+</div>
+
+<span>JavaScrispant</span>
+<div class="container">
+  <div class="skills js">65%</div>
+</div>
+
+<span>spanHspan</span>
+<div class="container">
+  <div class="skills spanhp">60%</div>
+</div>
+            </div>
+        </div>
+        <hr>
+    </div> 
+
+  
+    <?php include 'PartOfWeb/Information_container.php'?>
+    <?php include 'PartOfWeb/footer.php'?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
