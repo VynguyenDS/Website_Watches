7ac0d3da7eb1c6a046f66cd65fd538111ea46ac0
HungVyDS <62836191+VynguyenDS@users.noreply.github.com>
2020-05-23 14:24:44 +0700

    orderProduct
    1000% Desgin

--- File_PHP/MainpageAdmin.php
+++ File_PHP/MainpageAdmin.php
@@ -75,7 +75,7 @@
 
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="Men.php">
+                    <a class="nav-link" href="Admin/InformOrder.php">
                           <span><b>THÔNG TIN ĐẶT HÀNG</b></span>
                     </a>
                 </li>
