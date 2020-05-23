3accccd8d46ad62bc12c835fba91b5d857bc5a17
khoa29992 <62530224+khoa29992@users.noreply.github.com>
2020-05-23 13:25:19 +0700

    edit

--- File_PHP/OrderProduct.php
+++ File_PHP/OrderProduct.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <?php 
-require("../DataBase/database.php");
+require('../DataBase/database.php');
 mysqli_set_charset($database,'utf8');
 $products ="";
 if(isset($_COOKIE['Customer'])){
@@ -295,11 +295,19 @@ style="color: white;margin-top: 10px;background-color: rgb(11,123,193)" onclick=
             alert("Sản phẩm đã thêm vào giỏ")
      }
     </script>
-
+<!--testting github-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<!--testting github-->
+
+<!--testting github-->
+
+<!--testting github-->
+
+<!--testting github-->
 
 <!--testting github-->
